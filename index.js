a6d0b403d453c1417180ea5f28d4a0d7bb509ac7
huutu289 <71864293+huutu289@users.noreply.github.com>
2021-02-18 16:32:14 +0700

    impove eat method for cat

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var Dog = require('./Dog');
 var Cat = require('./Cat');
+var Mouse = require('./Mouse');
+
+
 var dog = new Dog('Tom');
-var jerry =  new Cat();
-dog.eat(jerry);
+var tom =  new Cat();
+var mouse = new Mouse('Mickey');
+dog.eat(tom);
 console.log(dog);
 dog.sayHi();
+
+tom.eat(mouse);
+console.log(tom);
+
+try{
+    tom.eat(dog);
+
+}catch (error){
+console.log('Error while cat eating a dog');
+}
