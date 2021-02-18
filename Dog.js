8689e3d2ef881625489c3809f4272ca7da8b1b12
huutu289 <71864293+huutu289@users.noreply.github.com>
2021-02-18 09:33:14 +0700

    Add run and eat method for dog

--- Dog.js
+++ Dog.js
@@ -1,3 +1,9 @@
 function Dog(){
-    
+    this.stomach =[];
+}
+Dog.prototype.eat = function(Cat){
+    this.stomach.push(Cat);
+}
+Dog.prototype.run = function(){
+    console.log("dog is running");
 }
